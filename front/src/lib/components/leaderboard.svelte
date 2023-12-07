0de42e32562cc98288fb444a9a680de92c95ea74
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-12-07 23:42:21 +0100

    ✨  add leaderboard component and update page layout

---
+++ front/src/lib/components/leaderboard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
+
+  const sourceData = [
+    { username: 'kuruyia', score: 10811 },
+    { username: 'john.doe', score: 6231 },
+    { username: 'noetarbouriech', score: 5742 },
+    { username: 'assstra', score: 4122 },
+    { username: 'lapsus-ord', score: 10 },
+  ];
+
+  const tableSimple: TableSource = {
+    head: ['Username', 'Score'],
+    body: tableMapperValues(sourceData, ['username', 'score']),
+    meta: tableMapperValues(sourceData, ['username', 'score']),
+  };
+</script>
+
+<div class="w-full">
+  <Table source={tableSimple} />
+</div>
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,41 +1,20 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+  import Leaderboard from '$lib/components/leaderboard.svelte';
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Chat Noir FTW</h2>
-	</div>
+  <div class="space-y-10 text-center flex flex-col items-center w-full">
+    <h2 class="h2">Chat Noir FTW</h2>
+    <button type="button" class="btn btn-xl variant-filled">Jouer</button>
+    <section class="leaderboard-section">
+      <Leaderboard />
+    </section>
+  </div>
 </div>
 
 <style lang="postcss">
-	figure {
-		@apply flex relative flex-col;
-	}
-	figure svg,
-	.img-bg {
-		@apply w-64 h-64 md:w-80 md:h-80;
-	}
-	.img-bg {
-		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
-			glow 5s linear infinite;
-	}
-	@keyframes glow {
-		0% {
-			@apply bg-primary-400/50;
-		}
-		33% {
-			@apply bg-secondary-400/50;
-		}
-		66% {
-			@apply bg-tertiary-400/50;
-		}
-		100% {
-			@apply bg-primary-400/50;
-		}
-	}
-	@keyframes pulse {
-		50% {
-			transform: scale(1.5);
-		}
-	}
+  .leaderboard-section {
+    width: 100%;
+    max-width: 600px;
+  }
 </style>
