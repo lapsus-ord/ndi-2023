a6e605f8d40199f43f5d3bc5d08c0fa6bcf7adb0
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-12-07 22:32:12 +0100

    🎨 eslint/prettier format

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,31 +1,23 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-	export const ssr = false;
-	export const csr = true;
-	export const prerender = false;
+  import '../app.postcss';
+  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+  export const ssr = false;
+  export const csr = true;
+  export const prerender = false;
 </script>
 
-<!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">🐈‍⬛ Chat Noir Project</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/lapsus-ord/ndi-2023"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+  <svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead">
+        <strong class="text-xl uppercase">🐈‍⬛ Chat Noir Project</strong>
+      </svelte:fragment>
+      <svelte:fragment slot="trail">
+        <a class="btn btn-sm variant-ghost-surface" href="https://github.com/lapsus-ord/ndi-2023" target="_blank" rel="noreferrer"> GitHub </a>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+
+  <slot />
 </AppShell>
