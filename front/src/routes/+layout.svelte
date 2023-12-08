d408898acaf10030fcb64a58162b247a373de7fb
Adrien Raimbault <adrien.raimbault@etu.umontpellier.fr>
2023-12-08 04:18:50 +0100

    ✨ introduce changing theme

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
   import '../app.postcss';
-  import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+  import { AppShell, AppBar, RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
   export const ssr = false;
   export const csr = true;
   export const prerender = false;
+
+  function changePreset(e: Event) {
+    document.body.setAttribute('data-theme', e.target.value)
+  }
+
+  function setBodyThemeAttribute(): void {}
+  setBodyThemeAttribute();
+
+  let value: string = 'skeleton';
 </script>
 
 <AppShell>
@@ -14,6 +23,12 @@
         <strong class="text-xl uppercase">🐈‍⬛ Chat Noir Project</strong>
       </svelte:fragment>
       <svelte:fragment slot="trail">
+        <RadioGroup>
+          <RadioItem bind:group={value} name="justify" value='skeleton' on:click={(e) => changePreset(e)}>skeleton</RadioItem>
+          <RadioItem bind:group={value} name="justify" value='crimson' on:click={(e) => changePreset(e)}>crimson</RadioItem>
+          <RadioItem bind:group={value} name="justify" value='wintry' on:click={(e) => changePreset(e)}>wintry</RadioItem>
+          <RadioItem bind:group={value} name="justify" value='modern' on:click={(e) => changePreset(e)}>modern</RadioItem>
+        </RadioGroup>
         <a class="btn btn-sm variant-ghost-surface" href="https://github.com/lapsus-ord/ndi-2023" target="_blank" rel="noreferrer"> GitHub </a>
       </svelte:fragment>
     </AppBar>
